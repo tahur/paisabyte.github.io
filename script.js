c890b9d380c9147ca993187c0f7400b69cd62e1e
Mohammed Tahur <mdtahur@gmail.com>
2022-04-03 21:29:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,108 @@
+// 'use strict';
+
+// /////////////////////////////////////////////////
+// /////////////////////////////////////////////////
+// // BANKIST APP
+
+const formsList = [
+  ['qty-1', 'price-1'],
+  ['qty-2', 'price-2'],
+];
+
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const data = [];
+document.getElementById('btn--lg').addEventListener('click', function () {
+  console.log('cliked');
+  formsList.forEach(form => {
+    const price = Number(document.querySelector(`.${form[0]}`).value);
+    const quantiy = Number(document.querySelector(`.${form[1]}`).value);
+    data.push([price, quantiy]);
+  });
+  const totalQty = data.reduce((acc, a, i, arry) => {
+    console.log(arry, a[1]);
+    return acc + a[0];
+  }, 0);
+  const totalvalue = data.map(arr => arr[0] * arr[1]);
+
+  const totalAmount = totalvalue.reduce((acc, val) => acc + val, 0);
+
+  const totalAvg = totalvalue.reduce((acc, val) => acc + val / totalQty, 0);
+
+  labelBalance.textContent = totalAvg.toFixed(2);
+  labelSumIn.textContent = totalAmount;
+  labelSumOut.textContent = totalQty;
+  console.log(totalvalue, totalQty, totalAvg);
+});
+
+// // Data
+// const account1 = {
+//   owner: 'Jonas Schmedtmann',
+//   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+//   interestRate: 1.2, // %
+//   pin: 1111,
+// };
+
+// const account2 = {
+//   owner: 'Jessica Davis',
+//   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+//   interestRate: 1.5,
+//   pin: 2222,
+// };
+
+// const account3 = {
+//   owner: 'Steven Thomas Williams',
+//   movements: [200, -200, 340, -300, -20, 50, 400, -460],
+//   interestRate: 0.7,
+//   pin: 3333,
+// };
+
+// const account4 = {
+//   owner: 'Sarah Smith',
+//   movements: [430, 1000, 700, 50, 90],
+//   interestRate: 1,
+//   pin: 4444,
+// };
+
+// const accounts = [account1, account2, account3, account4];
+
+// // Elements
+// const labelWelcome = document.querySelector('.welcome');
+// const labelDate = document.querySelector('.date');
+// const labelBalance = document.querySelector('.balance__value');
+// const labelSumIn = document.querySelector('.summary__value--in');
+// const labelSumOut = document.querySelector('.summary__value--out');
+// const labelSumInterest = document.querySelector('.summary__value--interest');
+// const labelTimer = document.querySelector('.timer');
+
+// const containerApp = document.querySelector('.app');
+// const containerMovements = document.querySelector('.movements');
+
+// const btnLogin = document.querySelector('.login__btn');
+// const btnTransfer = document.querySelector('.form__btn--transfer');
+// const btnLoan = document.querySelector('.form__btn--loan');
+// const btnClose = document.querySelector('.form__btn--close');
+// const btnSort = document.querySelector('.btn--sort');
+
+// const inputLoginUsername = document.querySelector('.login__input--user');
+// const inputLoginPin = document.querySelector('.login__input--pin');
+// const inputTransferTo = document.querySelector('.form__input--to');
+// const inputTransferAmount = document.querySelector('.form__input--amount');
+// const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+// const inputCloseUsername = document.querySelector('.form__input--user');
+// const inputClosePin = document.querySelector('.form__input--pin');
+
+// /////////////////////////////////////////////////
+// /////////////////////////////////////////////////
+// // LECTURES
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// /////////////////////////////////////////////////
