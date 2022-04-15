d74cab3c9d62828a5e53ba69d84443992334f5f1
Mohammed Tahur <mdtahur@gmail.com>
2022-04-15 14:39:59 +0530

    Logic Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,12 @@ const addElement = document.querySelector('.btn-add-row');
 const tradeList = document.querySelector('.trade-list');
 const btnReset = document.querySelector('.btn-reset');
 const btnClear = document.querySelector('.btn-clear');
-const tradeRow = document.querySelectorAll('.trade-input');
+const tradeInputs = document.getElementsByClassName('trade-input');
+const tradeCalc = document.querySelector('.trade-calc-btn');
+const tradeAvg = document.querySelector('.trade-avg');
+const totalQtyEle = document.querySelector('.total-qty');
+const totalValueEle = document.querySelector('.total-value');
+const tradeRow = document.getElementsByClassName('trade-item-row');
 
 let tradeCounter = 3;
 let qty = 3;
@@ -39,5 +44,57 @@ btnReset.addEventListener('click', () => location.reload(true));
 // });
 
 // tradeRow.forEach(e => console.log(e));
+// let totalQty = 0;
+// let totalValue = 0;
+// let totalAvg = 0;
+function inputValues() {
+  for (let i = 0; i < tradeInputs.length; i++) {
+    tradeInputs[i].value = '';
+  }
+}
 
-let data = [];
+let data = {};
+
+function updatingCalcValues() {
+  const dataValue = Object.values(data);
+  const totalQty = dataValue.reduce((acc, item) => {
+    return acc + Number(item[0]);
+  }, 0);
+
+  const amountLists = dataValue.map(arr => Number(arr[0] * arr[1]));
+
+  const totalValue = amountLists.reduce((acc, val) => acc + val, 0);
+
+  const totalAvg = amountLists.reduce((acc, val) => acc + val / totalQty, 0);
+  console.log(dataValue, totalQty, totalValue, amountLists, totalAvg);
+  //
+  tradeAvg.textContent = new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+  }).format(totalAvg);
+  // const totalAmount = totalvalue.reduce((acc, val) => acc + val, 0);
+  totalQtyEle.textContent = totalQty;
+  totalValueEle.textContent = totalValue;
+  // ;
+
+  // labelBalance.textContent = totalAvg.toFixed(2);
+  // labelSumIn.textContent = totalAmount;
+  // labelSumOut.textContent = totalQty;
+}
+
+function dataHandler() {
+  data = {};
+  for (let i = 0; i < tradeRow.length; i++) {
+    const tradeName = tradeRow[i].children[0].textContent;
+    const qty = tradeRow[i].children[1].value;
+    const price = tradeRow[i].children[2]?.value;
+    data[tradeName] = [qty, price];
+  }
+  updatingCalcValues();
+}
+
+//Clearing the fileds without removing the added row
+btnClear.addEventListener('click', inputValues);
+
+let datary = [];
+tradeCalc.addEventListener('click', dataHandler);
