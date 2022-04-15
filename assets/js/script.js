b8f131188e8e17c6c30d7f5315770fdf965042d4
Mohammed Tahur <mdtahur@gmail.com>
2022-04-15 17:20:48 +0530

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,71 +3,34 @@ const tradeList = document.querySelector('.trade-list');
 const btnReset = document.querySelector('.btn-reset');
 const btnClear = document.querySelector('.btn-clear');
 const tradeInputs = document.getElementsByClassName('trade-input');
-const tradeCalc = document.querySelector('.trade-calc-btn');
+const tradeCalcBtn = document.querySelector('.trade-calc-btn');
 const tradeAvg = document.querySelector('.trade-avg');
 const totalQtyEle = document.querySelector('.total-qty');
 const totalValueEle = document.querySelector('.total-value');
 const tradeRow = document.getElementsByClassName('trade-item-row');
+const errorMsg = document.querySelector('.error');
 
-let tradeCounter = 3;
-let qty = 3;
-let price = 3;
+//Intial States
+let tradeTagCounter = 3;
+let qtyTagCounter = 3;
+let priceTagCounter = 3;
+let data = {};
+
 //Adding new row to the list
 addElement.addEventListener('click', function () {
   const html = `<li class="trade-item-row">
-    <p class="trade-tag">Trade ${tradeCounter}</p>
-    <input type="number" placeholder="Quantity" class="trade-input qty-${qty}" />
-    <input type="number" placeholder="Price" class="trade-input price-${price}" />
+    <p class="trade-tag">Trade ${tradeTagCounter}</p>
+    <input type="number" placeholder="Quantity" class="trade-input qty-${qtyTagCounter}" />
+    <input type="number" placeholder="Price" class="trade-input price-${priceTagCounter}" />
   </li>`;
 
   tradeList.insertAdjacentHTML('beforeend', html);
   tradeCounter++;
   qty++;
   price++;
-  console.log(tradeRow);
 });
 
-//Restting the whole page
-
-btnReset.addEventListener('click', () => location.reload(true));
-
-//Clearing the input filed
-
-// btnClear.addEventListener('click', () => {
-//   tradeRow.forEach((e, i) => {
-//     console.log(e, i);
-//     // const elements = e.getElementsByClassName('trade-input');
-//     // console.log(elements);
-//     // console.log(elements[0].value);
-//     // console.log(elements[1].value);
-//   });
-// });
-
-// tradeRow.forEach(e => console.log(e));
-// let totalQty = 0;
-// let totalValue = 0;
-// let totalAvg = 0;
-function inputValues() {
-  for (let i = 0; i < tradeInputs.length; i++) {
-    tradeInputs[i].value = '';
-  }
-}
-
-let data = {};
-
-function updatingCalcValues() {
-  const dataValue = Object.values(data);
-  const totalQty = dataValue.reduce((acc, item) => {
-    return acc + Number(item[0]);
-  }, 0);
-
-  const amountLists = dataValue.map(arr => Number(arr[0] * arr[1]));
-
-  const totalValue = amountLists.reduce((acc, val) => acc + val, 0);
-
-  const totalAvg = amountLists.reduce((acc, val) => acc + val / totalQty, 0);
-  console.log(dataValue, totalQty, totalValue, amountLists, totalAvg);
-  //
+function updateUI(totalAvg = 00, totalQty = '--', totalValue = '--') {
   tradeAvg.textContent = new Intl.NumberFormat('en-IN', {
     style: 'currency',
     currency: 'INR',
@@ -75,13 +38,28 @@ function updatingCalcValues() {
   // const totalAmount = totalvalue.reduce((acc, val) => acc + val, 0);
   totalQtyEle.textContent = totalQty;
   totalValueEle.textContent = totalValue;
-  // ;
-
-  // labelBalance.textContent = totalAvg.toFixed(2);
-  // labelSumIn.textContent = totalAmount;
-  // labelSumOut.textContent = totalQty;
+  errorMsg.textContent = '';
 }
 
+function updatingCalcValues() {
+  const inputEntriesList = Object.values(data);
+
+  if (inputEntriesList.flat().includes('')) {
+    errorMsg.textContent = '! Input cannot left blank';
+  } else {
+    const totalQty = inputEntriesList.reduce((acc, item) => {
+      return acc + Number(item[0]);
+    }, 0);
+    const amountLists = inputEntriesList.map(arr => Number(arr[0] * arr[1]));
+
+    const totalValue = amountLists.reduce((acc, val) => acc + val, 0);
+
+    const totalAvg = amountLists.reduce((acc, val) => acc + val / totalQty, 0);
+    updateUI(totalAvg, totalQty, totalValue);
+  }
+}
+
+//The dataHandler extracts value from input filed and add to data object
 function dataHandler() {
   data = {};
   for (let i = 0; i < tradeRow.length; i++) {
@@ -93,8 +71,16 @@ function dataHandler() {
   updatingCalcValues();
 }
 
-//Clearing the fileds without removing the added row
-btnClear.addEventListener('click', inputValues);
+//Restting the whole page
+btnReset.addEventListener('click', () => location.reload(true));
 
-let datary = [];
-tradeCalc.addEventListener('click', dataHandler);
+//Clear button handler
+btnClear.addEventListener('click', () => {
+  for (let i = 0; i < tradeInputs.length; i++) {
+    tradeInputs[i].value = '';
+  }
+  updateUI();
+});
+
+//Calculate button
+tradeCalcBtn.addEventListener('click', dataHandler);
